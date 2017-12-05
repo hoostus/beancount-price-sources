7ccc25823b751a1161ac5184d8160bb094a12d73
Justus Pendleton <justus@ego-pulse.com>
2017-12-05 14:10:33 +0700

    fix typos

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from disutils.core import setup
+from distutils.core import setup
 
 setup(
         name='beancount-hoostus-prices',
         version='1.0',
-        packages=['bc'],
+        packages=['hoostus_sources'],
         license='BSD',
         long_description=open('README.md').read(),
 )
